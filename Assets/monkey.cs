3961b27d6bdcc7f9dd20af3881a614ef403fd893
Isaac <isaacrivera2020@u.northwestern.edu>
2020-02-10 19:18:08 -0600

    monkey cna move left and right

---
+++ Assets/monkey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monkey : MonoBehaviour
+{
+    public Rigidbody2D monkey_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            monkey_rb.MovePosition(monkey_rb.position + Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            monkey_rb.MovePosition(monkey_rb.position + Vector2.right);
+        }
+    }
+}
