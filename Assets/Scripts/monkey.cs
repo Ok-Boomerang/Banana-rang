ebb97c4debb0348518b582006d37fb149a189130
Danielle Chamberlain <089daniellechambers@gmail.com>
2020-02-11 19:22:36 -0600

    folder clean ups, additional monkey motion, addition of boomerang motion and colliders

--- Assets/Scripts/monkey.cs
+++ Assets/Scripts/monkey.cs
@@ -1,27 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class monkey : MonoBehaviour
 {
     public Rigidbody2D monkey_rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             monkey_rb.MovePosition(monkey_rb.position + Vector2.left);
         }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             monkey_rb.MovePosition(monkey_rb.position + Vector2.right);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            monkey_rb.MovePosition(monkey_rb.position + Vector2.up * 1.5f );
         }
     }
 }
