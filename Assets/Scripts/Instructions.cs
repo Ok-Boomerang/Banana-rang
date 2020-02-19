51c76716e6258c0b3ca9e4882f4a57e72b80a835
anuraife <anuradharaife2020@u.northwestern.edu>
2020-02-19 16:53:14 -0600

    Isaac's power bar and updated scoring/instruction level

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Instructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        var buttons = transform.Find("Buttons");
+
+        var startbtn = transform.Find("Start").GetComponent<Button>();
+        startbtn.onClick.AddListener(StartGame);
+    }
+    private void StartGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
