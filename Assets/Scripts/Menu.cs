7deeb6119e52bc05bbb9ff87e5f648e2b7a20bbf
Danielle Chamberlain <089daniellechambers@gmail.com>
2020-02-19 22:57:39 -0600

    Menu stuff

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,33 +8,20 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Texture2D cursor;
-    public Image fade;
     // Start is called before the first frame update
      private void Awake () {
-            var buttons = transform.Find("Buttons");
-    
-            var startbtn = buttons.Find("Start").GetComponent<Button>();
-            startbtn.onClick.AddListener(StartGame);
-    
-            var levelsbtn = buttons.Find("Levels").GetComponent<Button>();
-            levelsbtn.onClick.AddListener(ShowLevels);
-            
-            var instructionsbtn = buttons.Find("Instructions").GetComponent<Button>();
-            instructionsbtn.onClick.AddListener(ShowInstructions);
-    
-            var quitbtn = buttons.Find("Quit").GetComponent<Button>();
-            quitbtn.onClick.AddListener(QuitGame);
             var xspot = cursor.width / 2;
             var yspot = cursor.height / 2;
             Vector2 hotSpot = new Vector2(xspot,yspot);
             Cursor.SetCursor(cursor, hotSpot, CursorMode.Auto);
-        }
-     private void StartGame () {
+     }
+
+     public static void StartGame () {
          SceneManager.LoadScene(3);
      }
      
 
-     private void QuitGame () {
+     public static void QuitGame () {
 #if UNITY_EDITOR
          EditorApplication.isPlaying = false;
 #else
@@ -42,10 +29,10 @@ public class Menu : MonoBehaviour
 #endif
      }
 
-     private void ShowLevels () {
+     public static void ShowLevels () {
          SceneManager.LoadScene(1);
      }
-     private void ShowInstructions () {
+     public static void ShowInstructions () {
          SceneManager.LoadScene(2);
      }
 }
---
+++ Assets/Scripts/buttonhelper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class buttonhelper : Selectable
+{
+
+    public GameObject visual;
+
+    void Update()
+    {
+        visual.GetComponent<SpriteRenderer>().enabled = IsHighlighted() == true;
+        if (!IsPressed()) return;
+        switch (gameObject.name)
+        {
+            case "Start":
+                Menu.StartGame();
+                break;
+            case "Levels":
+                Menu.ShowLevels();
+                break;
+            case "Instructions":
+                Menu.ShowInstructions();
+                break;
+            case "Quit":
+                Menu.QuitGame();
+                break;
+        }
+    }
+    
+    
+    
+}
